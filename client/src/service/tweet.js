a76b93e2ea79af949d9258800d8b1bafdc706d2c
Jisoo <k3ji0706@gmail.com>
2022-12-06 03:30:58 +0900

    13강 리팩토링

--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -1,54 +1,32 @@
 export default class TweetService {
-  constructor(baseURL) {
-    this.baseURL = baseURL;
+  constructor(http) {
+    this.http = http;
   }
 
   async getTweets(username) {
     const query = username ? `?username=${username}` : '';
-    const respose = await fetch(`${this.baseURL}/tweets${query}`, {
+    return this.http.fetch(`/tweets${query}`, {
       method: 'GET',
-      headers: { 'Content-type': 'application/json' },
     });
-    const data = await respose.json();
-    if(respose.status !== 200) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 
   async postTweet(text) {
-    const respose = await fetch(`${this.baseURL}/tweets`, {
+    return this.http.fetch(`/tweets`, {
       method: 'POST',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify({ text, username: 'js', name: 'js '}),
+      body: JSON.stringify({ text, username: 'ellie', name: 'Ellie' }),
     });
-    const data = await respose.json();
-    if(respose.status !== 201) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 
   async deleteTweet(tweetId) {
-    const respose = await fetch(`${this.baseURL}/tweets${tweetId}`, {
+    return this.http.fetch(`/tweets/${tweetId}`, {
       method: 'DELETE',
-      headers: { 'Content-type': 'application/json' },
     });
-    if(respose.status !== 204) {
-      throw new Error();
-    }
   }
 
   async updateTweet(tweetId, text) {
-    const respose = await fetch(`${this.baseURL}/tweets${tweetId}`, {
+    return this.http.fetch(`/tweets/${tweetId}`, {
       method: 'PUT',
-      headers: { 'Content-type': 'application/json' },
       body: JSON.stringify({ text }),
     });
-    const data = await respose.json();
-    if(respose.status !== 201) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 }
