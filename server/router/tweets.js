a4ca694e83650d1d6cd14a43ccd0ff39abd6099b
Jisoo <k3ji0706@gmail.com>
2022-12-06 04:57:30 +0900

    14강 validation

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,7 +1,17 @@
 import express from "express";
 import "express-async-errors";
-import * as tweetController from "../controller/tweet.js"
+import { body } from "express-validator";
+import * as tweetController from "../controller/tweet.js";
+import { validate } from "../middleware/validator.js";
 const router = express.Router();
+
+const validateTweet = [
+  body('text')
+    .trim()
+    .isLength({min: 3})
+    .withMessage('3글자 이상 입력해주세요'),
+  validate,
+]
 
 // GET /tweets
 // GET /tweets?username=:username
@@ -11,10 +21,10 @@ router.get('/', tweetController.getTweets);
 router.get('/:id', tweetController.getTweet);
 
 // POST /tweets
-router.post('/', tweetController.createTweet);
+router.post('/', validateTweet, tweetController.createTweet);
 
 // PUT /tweets/:id
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', validateTweet, tweetController.updateTweet);
 
 // DELETE /tweets/:id
 router.delete('/:id', tweetController.deleteTweet);
