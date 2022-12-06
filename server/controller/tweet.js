b7815f4067cbfc423b98c34746a9ee586321b867
Jisoo <k3ji0706@gmail.com>
2022-12-06 22:49:07 +0900

    15강 회원가입,로그인

--- server/controller/tweet.js
+++ server/controller/tweet.js
@@ -2,9 +2,11 @@ import * as tweetRepository from "../data/tweet.js"
 
 export async function getTweets(req, res, next) {
   const username = req.query.username;
-  const data = await (username
+  const data = await (
+    username
     ? tweetRepository.getAllByUsername(username)
-    : tweetRepository.getAll());
+    : tweetRepository.getAll()
+    );
   res.status(200).json(data);
 }
 
@@ -27,16 +29,26 @@ export async function createTweet(req, res, next) {
 export async function updateTweet(req, res, next) {
   const id = req.params.id;
   const text = req.body.text;
-  const tweet = await tweetRepository.update(id, text);
-  if (tweet) {
-    res.status(200).json(tweet);
-  } else {
-    res.status(404).json({ message: `tweet id(${id}) not found` });
+  const tweet = await tweetRepository.getById(id);
+  if (!tweet) {
+    return res.sendStatus(404);
   }
+  if (tweet.userId !== req.userId) {
+    return res.sendStatus(403);
+  }
+  const updated = await tweetRepository.update(id, text);
+  res.status(200).json(updated)
 }
 
 export async function deleteTweet(req, res, next) {
   const id = req.params.id;
+  const tweet = await tweetRepository.getById(id);
+  if (!tweet) {
+    return res.sendStatus(404);
+  }
+  if (tweet.userId !== req.userId) {
+    return res.sendStatus(403);
+  }
   await tweetRepository.remove(id);
   res.sendStatus(204);
 }
